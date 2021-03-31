447f98e01b9f7f5a554f50148bf614c76695b95d
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-03-30 20:53:56 -0500

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // Assignment Code
+var charactersLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var charactersUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var charactersNumbers = '0123456789';
+var charactersSpecials = '-*/\][}{.,/?!@#$%^&*;:';
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -6,31 +10,70 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  alert("Welcome");
 }
 
-// Add event listener to generate button
-<button generate = "writePassword(length)" ></button>
+function writePassword() {
+  console.log('hello');
+  var password = generatePassword();
+}
 
+function generatePassword() {
+  var options = getOptions();
+  var result = [];
+  var possibleCharacters = [];
+  var guarenteedCharacters = [];
 
-
-function writePassword(length) {
-  var password = '';
-  var charactersLower = 'abcdefghijklmnopqrstuvwxyz';
-  var charactersUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var charactersNumbers = '0123456789';
-  var charactersSpecials = '-*/\][}{.,/?!@#$%^&*;:';
-  var passwordLength = prompt("8-128 characters")
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  if (options.specials){
+    possibleCharacters = possibleCharacters.concat(charactersSpecials);
+    guarenteedCharacters.push(getrandom(charactersSpecials))
   }
-  return result;
+
+};
+function getRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length)
+  var randomElement = arr[randomIndex]
+  return randomElement;
 }
 
 
-//Create an array of lowercase letters
-//Create ana array of uppder case letters
-//create an array of numbers
-//create and array of special characters
-//
+function getOptions() {
+  var length = parseInt(prompt("How many characters would you like your password to contain?"));
+  console.log(length)
+  if (isNaN(length)) {
+    alert("Input must be a number between 8 and 128.")
+    return;
+  }
+  if (length < 8 || length > 128) {
+    alert("Input must be a number between 8 and 128.")
+    return;
+  }
+var specials = confirm("Would you like to have special characters?")
+var uppers = confirm("Would you like to have uppercase characters?")
+var lowers = confirm("Would you like to have lowercase characters?")
+var numericals = confirm("Would you like to have numerical characters?")
+
+if (specials === false && 
+  uppers === false &&
+  lowers === false &&
+  numericals === false
+  ){
+    alert("You must choose at least one type of characters.")
+    return;
+  }
+var passwordOptions = {
+  length: length,
+  specials: specials,
+  uppers: uppers,
+  lowers: lowers,
+  numericals: numericals
+}
+return passwordOptions;
+}
+
+generateBtn.addEventListener("click", writePassword);
+
+// if(isNaN(num1)){
+//   document.write(num1 + " is not a number <br/>");
+// }else{
+//   document.write(num1 + " is a number <br/>");
+// }
